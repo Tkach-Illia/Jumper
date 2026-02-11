15b7724f2d74ce9b2e7511544f3db10eb6f45a85
Tkach Illia <tkachillia00@gmail.com>
2026-02-11 10:10:12 +0200

    Added jumping and moving platforms

---
+++ scripts/platform.gd
@@ -0,0 +1,29 @@
+extends AnimatableBody2D
+
+@export var curve: Curve
+
+@export var left_offset: float = -10
+@export var right_offset: float = 10
+@export var speed: float = .1
+
+var direction: bool = true
+var pos: float = .0
+var starting_pos: Vector2 = Vector2(0, 0)
+
+func _ready() -> void:
+	starting_pos.x = position.x
+	starting_pos.y = position.y
+
+func _process(delta: float) -> void:
+	if direction:
+		pos += speed * delta
+	else:
+		pos -= speed * delta
+	
+	if curve.sample(pos) == 1:
+		direction = false
+	elif curve.sample(pos) == 0:
+		direction = true
+		
+	print(curve.sample(pos))
+	global_position.x = (abs(right_offset) + abs(left_offset)) * curve.sample(pos) + starting_pos.x
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,12 +2,13 @@ extends CharacterBody2D
 
 var gravity : float = 200.0
 var speed : float = 200.0
+var bounce_force : float = 300.0
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
-		velocity.y = 0
+		velocity.y = -bounce_force 
 	
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -speed
